bfad78259007a98cd9963dd2a18c16b98f39ec23
Superkart <karthik.ragi15@gmail.com>
2025-07-23 15:41:30 -0500

    Call by address and call by reference done

--- AbdulBari_Cpp_Course/parameterPassingMethods.cpp
+++ AbdulBari_Cpp_Course/parameterPassingMethods.cpp
@@ -9,10 +9,37 @@ int CallByValueAdd(int a, int b)
     int sum = a + b;
     return sum;
 }
+
+void CallByAddressSwap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+
+}
+
+void CallByReference(int &a, int &b)
+{   
+    int temp = a;
+    a = b;
+    b = temp;
+}
 int main()
 {
 
     cout << CallByValueAdd(10, 20)<<endl;
+
+    int num1 = 20;
+    int num2 = 30;
+
+    CallByAddressSwap(&num1,&num2);
+    cout<< "num 1: " << num1 <<endl;
+    cout<< "num 2: " << num2 <<endl;
+
+    CallByReference(num1, num2);
+    cout<< "num 1: " << num1 <<endl;
+    cout<< "num 2: " << num2 <<endl;
+
 
 
 }
