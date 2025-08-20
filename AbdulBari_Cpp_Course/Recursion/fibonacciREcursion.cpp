ed72c184829ceb51fcd7a1df3e07a34844a49863
Superkart <karthik.ragi15@gmail.com>
2025-08-20 14:32:23 -0500

    fibonacci series using loop

---
+++ AbdulBari_Cpp_Course/Recursion/fibonacciREcursion.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int FibonacciItterative(int n)
+{
+    int sum = 0;
+    int t0, t1 = 1;
+
+    if(n<=1)
+    {
+        return n;
+    }
+    
+    for(int i=2; i<=n; i++)
+    {
+        sum = t0 + t1;
+        t0 = t1;
+        t1 = sum;
+    }
+
+    return sum;
+}
+
+int main()
+{
+    cout<<FibonacciItterative(10)<<endl;
+    return 0;
+}
