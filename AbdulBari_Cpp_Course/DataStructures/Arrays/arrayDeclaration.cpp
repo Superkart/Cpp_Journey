a0d9199256c5254a74792b41f6775292e8476727
Superkart <karthik.ragi15@gmail.com>
2025-08-31 19:52:31 -0500

    All the ways to initialise and decalre an array in CPP

---
+++ AbdulBari_Cpp_Course/DataStructures/Arrays/arrayDeclaration.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int A[5];
+    int B[5] = {1, 2, 3, 4, 5};
+    int C[10] = {1, 2, 3};
+    int D[5] = {0};
+    int E[] = {1, 2, 3, 4, 5, 6};
+   
+    return 0;
+}
