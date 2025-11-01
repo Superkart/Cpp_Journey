ccd21a7a01d267da010ec23730f75d0253447002
Superkart <karthik.ragi15@gmail.com>
2025-11-01 17:58:49 -0500

    actual map, set

--- AbdulBari_Cpp_Course/DataStructures/Hashmap/hashmap.cpp
+++ AbdulBari_Cpp_Course/DataStructures/Hashmap/hashmap.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <map>
 #include <set>
+#include <unordered_map>
 
 using namespace std;
 
 int main()
 {
     map<int, int> map1 = {{1,100}, {2,200}};
-    
+    set<int> set1;
+
     map1[6000] = 100;
     map1[3] = 300;
     for(auto i:map1)
@@ -27,6 +29,9 @@ int main()
 
     }
     map1.erase(3);
+    map1.insert({1,1});
+    set1.insert(1);
+    set1.erase(1);
     map1[4];
     cout<<map1[4];
 
